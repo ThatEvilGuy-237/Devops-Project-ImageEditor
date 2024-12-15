37d0bb681d0b383e2776a1c33ff70ceb4a2ac65b
Bart Brits <brits68@gmail.com>
2024-12-15 11:00:26 +0000

    Ref: Changed db configuration

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost:3306',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,   
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'image_editor',
